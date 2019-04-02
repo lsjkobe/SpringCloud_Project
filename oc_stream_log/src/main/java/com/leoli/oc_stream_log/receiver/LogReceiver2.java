3348dc222352cffd5700a99f4821b929c3962250
leoli <936339745@qq.com>
2019-04-03 00:24:50 +0800

    添加log4j2

--- oc_stream_log/src/main/java/com/leoli/oc_stream_log/receiver/LogReceiver2.java
+++ oc_stream_log/src/main/java/com/leoli/oc_stream_log/receiver/LogReceiver2.java
@@ -7,22 +7,38 @@ package com.leoli.oc_stream_log.receiver;// Copyright (c) 1998-2019 Core Solutio
 // ============================================================================
 
 import com.leoli.oc_stream_log.stream.messaging.ReceiveLog;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
+
 @Component
 @EnableBinding(value = ReceiveLog.class)
 public class LogReceiver2 {
+
+    Logger LOGGER = LogManager.getLogger(LogReceiver2.class);
+
+    //condition过滤，如果和headers中的version一样的话就接收
     @StreamListener(value = ReceiveLog.INPUT, condition = "headers['version']=='1.0'")
     public void input(Message message, @Header("version") String version){
         System.err.println("消息接收1:" + message.getPayload()+version);
     }
 
-    @StreamListener(value = ReceiveLog.INPUT, condition = "headers['version']=='2.0'")
-    public void input2(Message message, @Header("version") String version){
-        System.err.println("消息接收2:" + message.getPayload()+version);
+    @StreamListener(value = ReceiveLog.INPUT)
+    public void input2(Message<Map> message){
+        Map logInfo = message.getPayload();
+        String level = (String) logInfo.get("level");
+        String info = (String) logInfo.get("info");
+        this.log(level, info);
+    }
+
+    private void log(String level, String msg){
+        LOGGER.log(Level.toLevel(level), msg);
     }
 }
--- stream_producer/src/main/java/com/leoli/stream_producer/services/SendLogService.java
+++ stream_producer/src/main/java/com/leoli/stream_producer/services/SendLogService.java
@@ -7,10 +7,14 @@ package com.leoli.stream_producer.services;// Copyright (c) 1998-2019 Core Solut
 // ============================================================================
 
 import com.leoli.stream_producer.stream.messaging.SendLog;
+import org.slf4j.event.Level;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @EnableBinding(SendLog.class)
@@ -20,7 +24,10 @@ public class SendLogService {
     SendLog sendLog;
 
     public void send(String msg){
-        this.sendLog.output().send(MessageBuilder.withPayload(msg).setHeader("version","1.0").build());
-        this.sendLog.output().send(MessageBuilder.withPayload(msg).setHeader("version","2.0").build());
+        Map<String, Object> logInfo = new HashMap<>();
+        logInfo.put("level", Level.DEBUG.name());
+        logInfo.put("info", msg);
+        this.sendLog.output().send(MessageBuilder.withPayload(logInfo).setHeader("version","1.0").build());
+        this.sendLog.output().send(MessageBuilder.withPayload(logInfo).setHeader("version","2.0").build());
     }
 }
